b531009027e7ea2a76b47f055cd2e386c5d0a4cf
MitchellKaysen <kaysenmitchell@gmail.com>
2020-06-03 04:45:24 -0500

    That took me way longer than it should have (also it's incorrect still)
    Not very good at 3d math yet. Seems to work OK though, and that's whats important. Updating the actor rotation to the control rotation in actions can be done instantaneously for now, as it's not that important. Should look into making a 'Curve watcher' component or something so that I can drive animation using curve values and still plug into the action system

--- Source/GOAPProject/Private/LookAtTestController.cpp
+++ Source/GOAPProject/Private/LookAtTestController.cpp
@@ -1,7 +1,68 @@
 #include "..\Public\LookAtTestController.h"
 
-
 void ALookAtTestController::Tick(float DeltaTime)
 {
 	AActor::Tick(DeltaTime);
+	UpdateControlRotation(DeltaTime, bLockPawnToControlRotation);
+}
+void ALookAtTestController::UpdateControlRotation(float DeltaTime, bool bUpdatePawn)
+{
+	APawn* const MyPawn = GetPawn();
+	if (MyPawn)
+	{
+		FRotator NewControlRotation = GetControlRotation();
+
+		// Look toward focus
+		const FVector FocalPoint = GetFocalPoint();
+		if (FAISystem::IsValidLocation(FocalPoint))
+		{
+			NewControlRotation = (FocalPoint - MyPawn->GetPawnViewLocation()).Rotation();
+			if (bClampYaw)
+			{
+				float NewYaw = FRotator::ClampAxis(NewControlRotation.Yaw);
+				float CachedYaw = FRotator::NormalizeAxis(CachedRotation.Yaw);
+				float DeltaYaw = FRotator::NormalizeAxis(NewYaw - CachedYaw);
+
+				float ClampedDelta = FMath::Min(FMath::Max(DeltaYaw, -fHalfAngle), fHalfAngle);
+				NewControlRotation.Yaw = CachedYaw + ClampedDelta;
+				NewControlRotation.Clamp();
+			}
+		}
+		else if (bSetControlRotationFromPawnOrientation)
+		{
+			NewControlRotation = MyPawn->GetActorRotation();
+		}
+
+		// Don't pitch view unless looking at another pawn
+		if (NewControlRotation.Pitch != 0 && Cast<APawn>(GetFocusActor()) == nullptr)
+		{
+			NewControlRotation.Pitch = 0.f;
+		}
+
+		SetControlRotation(NewControlRotation);
+
+		if (bUpdatePawn)
+		{
+			const FRotator CurrentPawnRotation = MyPawn->GetActorRotation();
+
+			if (CurrentPawnRotation.Equals(NewControlRotation, 1e-3f) == false)
+			{
+				MyPawn->FaceRotation(NewControlRotation, DeltaTime);
+			}
+		}
+	}
+}
+
+void ALookAtTestController::LookAtActor(AActor* FocusTarget, bool bClampRotationToHalfAngle=true)
+{
+	CachedRotation = GetControlRotation();
+
+	SetFocus(FocusTarget);
+	bClampYaw = bClampRotationToHalfAngle;
+}
+
+void ALookAtTestController::StopLookAt()
+{
+	ClearFocus(EAIFocusPriority::Gameplay);
+	SetControlRotation(CachedRotation);
 }
--- Source/GOAPProject/Public/LookAtTestController.h
+++ Source/GOAPProject/Public/LookAtTestController.h
@@ -4,8 +4,6 @@
 #include "AIController.h"
 #include "LookAtTestController.generated.h"
 
-
-
 UCLASS(BlueprintType, Blueprintable)
 class GOAPPROJECT_API ALookAtTestController : public AAIController
 {
@@ -15,6 +13,23 @@ private:
 
 	UPROPERTY()
 		FRotator CachedRotation;
+
+	UPROPERTY()
+		bool bLockPawnToControlRotation = false;
+
+	UPROPERTY()
+		bool bClampYaw = false;
+
+	UPROPERTY(EditAnywhere)
+		float fHalfAngle = 60.0f;
 public:
 		virtual void Tick(float DeltaSeconds) override;
+
+		virtual void UpdateControlRotation(float DeltaTime, bool bUpdatePawn = true) override;
+
+		UFUNCTION(BlueprintCallable)
+		void LookAtActor(AActor* FocusTarget, bool bClampRotationToHalfAngle);
+
+		UFUNCTION(BlueprintCallable)
+		void StopLookAt();
 };
