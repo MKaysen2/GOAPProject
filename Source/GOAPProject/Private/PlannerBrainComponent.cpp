c464cddfbe1d7263495e45ffadf63025495cca26
MitchellKaysen <kaysenmitchell@gmail.com>
2020-06-08 19:05:25 -0500

    Adding planner braincomponent to centralize planner+goal logic (and possibly symbol services)
    This is basically like how BehaviorTree sublcasses its own braincomponent. I will also switch Actions to use braincomp messages which is brain-implementation-agnostic, so I can set that up first

---
+++ Source/GOAPProject/Private/PlannerBrainComponent.cpp
@@ -0,0 +1,1 @@
+#include "../Public/PlannerBrainComponent.h"
---
+++ Source/GOAPProject/Public/PlannerBrainComponent.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BrainComponent.h"
+#include "PlannerBrainComponent.generated.h"
+
+UCLASS()
+class UPlannerBrainComponent : public UBrainComponent
+{
+	GENERATED_BODY()
+
+};
